1c791f26f3694e5dc2f4af63a001efebac100538
Brendan Strejcek <brendan@datagazing.com>
2021-06-19 13:59:13 -0400

    initial commit

---
+++ tests/test_gleandoc.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python
+
+"""Tests for `gleandoc` package."""
+
+
+import unittest
+
+from gleandoc import gleandoc
+
+
+class TestGleandoc(unittest.TestCase):
+    """Tests for `gleandoc` package."""
+
+    def setUp(self):
+        """Set up test fixtures, if any."""
+
+    def tearDown(self):
+        """Tear down test fixtures, if any."""
+
+    def test_000_something(self):
+        """Test something."""
