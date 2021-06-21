29f5e4494031f70499dded293722b224a92a3c3e
Brendan Strejcek <brendan@datagazing.com>
2021-06-21 13:11:53 -0400

    Bump version: 0.1.0 → 0.2.0

--- gleandoc/__init__.py
+++ gleandoc/__init__.py
@@ -74,6 +74,6 @@ Limitations
 
 __author__ = """Brendan Strejcek"""
 __email__ = 'brendan@datagazing.com'
-__version__ = '0.1.0'
+__version__ = '0.2.0'
 
 from .gleandoc import docstring, main # noqa F401
