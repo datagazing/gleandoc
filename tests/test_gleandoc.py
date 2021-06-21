eafd7f7e737dd6149ad2b96fbfdcd702233f6279
Brendan Strejcek <brendan@datagazing.com>
2021-06-21 12:24:53 -0400

    fixed tests; updated tox to use -m pytest

--- tests/test_gleandoc.py
+++ tests/test_gleandoc.py
@@ -4,6 +4,10 @@
 
 
 import unittest
+import os
+import re
+import subprocess
+import sys
 
 import gleandoc
 
@@ -16,15 +20,35 @@ class TestGleandoc(unittest.TestCase):
 
     def tearDown(self):
         """Tear down test fixtures, if any."""
+        try:
+            os.unlink('test.rst')
+            print('tearDown: removed test.rst')
+        except Exception:
+            pass
 
     def test_docstring_stdlib_module(self):
         """Test extracting docstring from standard library module"""
         docstring = gleandoc.docstring('re')
-        first = docstring.splitlines()[0]
-        assert first == 'Support for regular expressions (RE).'
+        line = docstring.splitlines()[0]
+        assert line == 'Support for regular expressions (RE).'
 
     def test_docstring_cwd_basename(self):
         """Test extracting docstring based on working directory name"""
         docstring = gleandoc.docstring()
-        second = docstring.splitlines()[1]
-        assert second == 'Simple convenience function to extract docstring'
+        line = docstring.splitlines()[9]
+        # this test will need to be updated if the docstring changes
+        assert line == 'Print the first line of a docstring:'
+
+    def test_generate_readme(self):
+        """Test interpolating the docstring into readme at command line"""
+        # figure out how to test as installed
+        # determine tox env (this matches definitions in tox.ini, like 'py39')
+        toxenv = os.environ['TOX_ENV_NAME']
+        program = f"{os.getcwd()}/.tox/{toxenv}/bin/gleandoc"
+        subprocess.Popen(
+            [sys.executable, program, "README.rst.fstr", "test.rst"],
+        ).wait(timeout=30)
+        with open('test.rst') as f:
+            readme = f.read()
+            # look for a line that should be interpolated into the readme
+            assert bool(re.search(r'No dependencies', readme, re.MULTILINE))
